707abf311cd3df5e4d9cba3d2ee3a71b711714d3
jimi_zhangao <mail_zhangao@126.com>
2017-05-12 11:01:14 +0800

    修改代码

--- test_runner/test_runner_path.py
+++ test_runner/test_runner_path.py
@@ -1,8 +1,8 @@
 class TestRunnerPath(object):
-    def test_cases_path(self,name):
+    def test_cases_path(self, name):
         path = 'E:\\git\\tuqiangol_test\\testcases\\%s' % name
         return path
 
-    def test_report_path(self,name):
+    def test_report_path(self, name):
         path = '\\\\172.16.0.101\\share\\automate_report\\tuqiangol_test\\%s' % name
         return path

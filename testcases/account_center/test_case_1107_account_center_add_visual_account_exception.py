022fdeb1dc1e69ba246d0a00b637d2e79bd540f2
jimi_zhangao <mail_zhangao@126.com>
2017-06-26 18:58:11 +0800

    修改代码

---
+++ testcases/account_center/test_case_1107_account_center_add_visual_account_exception.py
@@ -0,0 +1,49 @@
+import csv
+import unittest
+from automate_driver.automate_driver_server import AutomateDriverServer
+from pages.account_center.account_center_page_read_csv import AccountCenterPageReadCsv
+from pages.account_center.account_center_visual_account_page import AccountCenterVisualAccountPage
+from pages.account_center.account_center_navi_bar_page import AccountCenterNaviBarPage
+from pages.base.base_page_server import BasePageServer
+from pages.base.lon_in_base_server import LogInBaseServer
+
+
+# 账户中心-虚拟账户管理---添加虚拟用户异常验证
+# author:戴招利
+class TestCase1107AccountCenterAddVisualEditException(unittest.TestCase):
+    def setUp(self):
+        self.driver = AutomateDriverServer()
+        self.base_url = self.driver.base_url
+        self.base_page = BasePageServer(self.driver, self.base_url)
+        self.visual_account_page = AccountCenterVisualAccountPage(self.driver, self.base_url)
+        self.account_center_page_navi_bar = AccountCenterNaviBarPage(self.driver, self.base_url)
+        self.account_center_page_read_csv = AccountCenterPageReadCsv()
+        self.log_in_base = LogInBaseServer(self.driver, self.base_page)
+        self.base_page.open_page()
+        self.driver.set_window_max()
+        self.driver.wait(1)
+        self.driver.clear_cookies()
+        self.driver.wait(1)
+
+    def test_add_visual_account_exception_validation(self):
+        '''添加虚拟用户验证'''
+
+        data = ["jimitest", "dzltest", "xnzh_cs01"]
+
+        # 登录
+        self.log_in_base.log_in()
+        # 进入虚拟账户管理
+        self.visual_account_page.enter_visual_account()
+        # 添加虚拟账号、保存
+        for i in range(len(data)):
+            self.visual_account_page.add_visual_account(data[i], "jimi123")
+            self.visual_account_page.save_add_info()
+            self.visual_account_page.dis_save_add_info()
+            self.driver.wait(1)
+            self.assertEqual("账号已存在", self.visual_account_page.get_save_status(), "在添加虚拟账号中可以添加已存在的账号！")
+            self.driver.wait(1)
+            # 退出登录
+            # self.account_center_page_navi_bar.usr_logout()
+
+    def tearDown(self):
+        self.driver.quit_browser()
---
+++ testcases/cust_manage/test_case_1109_cust_manage_add_virtual_user_expection.py
@@ -0,0 +1,67 @@
+import unittest
+from time import sleep
+
+from automate_driver.automate_driver_server import AutomateDriverServer
+from model.connect_sql import ConnectSql
+from pages.account_center.account_center_navi_bar_page import AccountCenterNaviBarPage
+from pages.base.base_page_server import BasePageServer
+from pages.base.lon_in_base_server import LogInBaseServer
+from pages.cust_manage.cust_manage_basic_info_and_add_cust_page import CustManageBasicInfoAndAddCustPage
+from pages.cust_manage.cust_manage_cust_list_page import CustManageCustListPage
+from pages.cust_manage.cust_manage_lower_account_page import CustManageLowerAccountPage
+from pages.cust_manage.cust_manage_my_dev_page import CustManageMyDevPage
+from pages.cust_manage.cust_manage_page_read_csv import CustManagePageReadCsv
+from pages.login.login_page import LoginPage
+
+
+# 新增客户客户数操作
+
+class TestCase1101CustManageCustSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = AutomateDriverServer()
+        self.base_url = self.driver.base_url
+        self.base_page = BasePageServer(self.driver, self.base_url)
+        self.login_page = LoginPage(self.driver, self.base_url)
+        self.cust_manage_basic_info_and_add_cust_page = CustManageBasicInfoAndAddCustPage(self.driver, self.base_url)
+        self.cust_manage_cust_list_page = CustManageCustListPage(self.driver, self.base_url)
+        self.cust_manage_my_dev_page = CustManageMyDevPage(self.driver, self.base_url)
+        self.cust_manage_lower_account_page = CustManageLowerAccountPage(self.driver, self.base_url)
+        self.account_center_page_navi_bar = AccountCenterNaviBarPage(self.driver, self.base_url)
+        self.driver.set_window_max()
+        self.log_in_base = LogInBaseServer(self.driver, self.base_url)
+        self.cust_manage_page_read_csv = CustManagePageReadCsv()
+        self.connect_sql = ConnectSql()
+        self.driver.wait(1)
+        self.driver.clear_cookies()
+        self.driver.wait(1)
+
+    def test_cust_manage_add_cust_search(self):
+        # 打开途强在线首页-登录页
+
+        self.base_page.open_page()
+        # 登录
+        self.log_in_base.log_in()
+
+        # 进入客户管理页面
+        self.cust_manage_basic_info_and_add_cust_page.enter_cust_manage()
+
+        self.cust_manage_basic_info_and_add_cust_page.add_acc()
+        self.cust_manage_basic_info_and_add_cust_page.cancel_add_account()
+        self.cust_manage_basic_info_and_add_cust_page.add_acc()
+
+        # 添加客户名称、账号,点击保存
+        self.cust_manage_basic_info_and_add_cust_page.add_account_name("虚拟账号")
+        self.cust_manage_basic_info_and_add_cust_page.add_account("xnzh_dzl02")
+        self.cust_manage_basic_info_and_add_cust_page.acc_add_save()
+
+        connect = self.connect_sql.connect_tuqiang_sql()
+        cursor = connect.cursor()
+        get_account_sql = "select account from user_info where account = 'xnzh_dzl02';"
+        cursor.execute(get_account_sql)
+        user = cursor.fetchall()
+        print(user)
+        print(len(user))
+        self.assertEqual(0, len(user), "创建普通账号，存在已存在的虚拟账户")
+
+    def tearDown(self):
+        self.driver.quit_browser()

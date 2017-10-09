9fb606b8d5ed354484ebb038d662bb8266ae5145
jimi_zhangao <mail_zhangao@126.com>
2017-10-09 10:40:57 +0800

    修改代码

--- change_data.py
+++ change_data.py
@@ -1,21 +1,15 @@
-------------------------------------------------------------------------
--- ddCopyNegate.decTest - - quiet
-decDouble
-copy and negate - -
--- Copyright(c)
-IBM
-Corporation, 1981, 2008.
-All
-rights
-reserved. - -
-------------------------------------------------------------------------
--- Please
-see
-the
-document
-"General Decimal Arithmetic Testcases" - -
--- at
-http: // www2.hursley.ibm.com / decimal
-for the description of      --
--- these
-testcases.
+class ChangeData(object):
+    # 为了切换测试和线上环境准备
+    def switch_tuqiang_url(self):
+        # 测试
+        # return 'http://172.16.0.116:8690'
+        return 'http://tujunsat.jimicloud.com'
+
+        # 线上
+        # return 'http://120.76.232.176:8180'
+        # return 'http://www.tuqiangol.com'
+
+        # 天眼在线测试
+        # return 'http://www.skyzaixian.com:8680'
+        # 天眼在线线上
+        # return 'http://www.skyzaixian.com'
--- pages/global_search/search_sql.py
+++ pages/global_search/search_sql.py
@@ -1,103 +1,79 @@
-try:
-    import _thread
-except ImportError:
-    _thread = None
-import importlib
-import os
-import os.path
-import shutil
-import sys
-from test import support
-import unittest
-import warnings
+class SearchSql(object):
+    def search_dev_sql(self, lower_account_tuple, search_data):
+        sql = "select d.imei from equipment_detail d inner join equipment_mostly m on d.imei = m.imei where m.userId in " + str(
+            lower_account_tuple)
 
-with warnings.catch_warnings():
-    warnings.simplefilter('ignore', PendingDeprecationWarning)
-    import imp
+        if search_data['dev_info'] != '':
+            sql += " and ( d.imei like '%" + search_data['dev_info'] + "%' or d.driverName like '%" + search_data[
+                'dev_info'] + "')"
 
+        sql += ";"
+        return sql
 
-def requires_load_dynamic(meth):
-    """Decorator to skip a test if not running under CPython or lacking
-    imp.load_dynamic()."""
-    meth = support.cpython_only(meth)
-    return unittest.skipIf(not hasattr(imp, 'load_dynamic'),
-                           'imp.load_dynamic() required')(meth)
+    def search_account_sql(self, lower_account_tuple, search_data):
+        # 全局搜索，搜索用户的sql
+        sql = "select o.id from user_info o where o.userId in " + str(lower_account_tuple)
 
+        if search_data['account_info'] != '':
+            sql += " and ( o.nickName like '%" + search_data['account_info'] + "%' or o.account like '%" + search_data[
+                'account_info'] + "%')"
 
-@unittest.skipIf(_thread is None, '_thread module is required')
-class LockTests(unittest.TestCase):
-    """Very basic test of import lock functions."""
+        sql += ";"
+        return sql
 
-    def verify_lock_state(self, expected):
-        self.assertEqual(imp.lock_held(), expected,
-                         "expected imp.lock_held() to be %r" % expected)
+    def search_complex_sql(self, user_id, search_data):
+        # 全局搜索 高级搜索
+        sql = "select d.imei from equipment_detail d inner join equipment_mostly m on d.imei = m.imei where m.userId = '%s'" % user_id
+        if search_data['base_info'] == 'imei':
+            sql += " and d.imei like '%" + search_data['info'] + "%'"
 
-    def testLock(self):
-        LOOPS = 50
+        elif search_data['base_info'] == '车牌号':
+            sql += " and d.vehicleNumber like '%" + search_data['info'] + "%'"
 
-        # The import lock may already be held, e.g. if the test suite is run
-        # via "import test.autotest".
-        lock_held_at_start = imp.lock_held()
-        self.verify_lock_state(lock_held_at_start)
+        elif search_data['base_info'] == '设备类型':
+            sql += " and m.mcType like '%" + search_data['info'] + "%'"
 
-        for i in range(LOOPS):
-            imp.acquire_lock()
-            self.verify_lock_state(True)
+        elif search_data['base_info'] == 'SN':
+            sql += " and d.sn like '%" + search_data['info'] + "%'"
 
-        for i in range(LOOPS):
-            imp.release_lock()
+        elif search_data['base_info'] == '车架号':
+            sql += " and d.carFrame like '%" + search_data['info'] + "%'"
 
-        # The original state should be restored now.
-        self.verify_lock_state(lock_held_at_start)
+        elif search_data['base_info'] == '设备SIM卡号':
+            sql += " and m.sim like '%" + search_data['info'] + "%'"
 
-        if not lock_held_at_start:
-            try:
-                imp.release_lock()
-            except RuntimeError:
-                pass
-            else:
-                self.fail("release_lock() without lock should raise "
-                          "RuntimeError")
+        elif search_data['base_info'] == '设备名称':
+            sql += " and m.deviceName like '%" + search_data['info'] + "%'"
 
+        begin_time = search_data['date'].split('/')[0]
+        end_time = search_data['date'].split('/')[1]
+        if search_data['is_date'] == '':
+            if search_data['date_type'] == '激活时间':
+                if begin_time != '':
+                    sql += " and DATEDIFF(m.activationTime,'" + begin_time + "') = 0"
 
-class ImportTests(unittest.TestCase):
-    def setUp(self):
-        mod = importlib.import_module('test.encoded_modules')
-        self.test_strings = mod.test_strings
-        self.test_path = mod.__path__
+            elif search_data['date_type'] == '平台到期时间':
+                if begin_time != '':
+                    sql += " and DATEDIFF(m.expiration,'" + begin_time + "') = 0"
 
-    def test_import_encoded_module(self):
-        for modname, encoding, teststr in self.test_strings:
-            mod = importlib.import_module('test.encoded_modules.'
-                                          'module_' + modname)
-            self.assertEqual(teststr, mod.test)
+        elif search_data['is_date'] == '1':
+            if search_data['date_type'] == '激活时间':
+                if begin_time != '':
+                    sql += " and m.activationTime >= '%s'" % begin_time
+                if end_time != '':
+                    sql += " and m.activationTime <= '%s'" % end_time
 
-    def test_find_module_encoding(self):
-        for mod, encoding, _ in self.test_strings:
-            with imp.find_module('module_' + mod, self.test_path)[0] as fd:
-                self.assertEqual(fd.encoding, encoding)
+            elif search_data['date_type'] == '平台到期时间':
+                if begin_time != '':
+                    sql += " and m.expiration >= '%s'" % begin_time
+                if end_time != '':
+                    sql += " and m.expiration <= '%s'" % end_time
 
-        path = [os.path.dirname(__file__)]
-        with self.assertRaises(SyntaxError):
-            imp.find_module('badsyntax_pep3120', path)
+        if search_data['arrearage'] != '':
+            sql += " and m.expiration <= CURDATE()"
 
-    def test_issue1267(self):
-        for mod, encoding, _ in self.test_strings:
-            fp, filename, info = imp.find_module('module_' + mod,
-                                                 self.test_path)
-            with fp:
-                self.assertNotEqual(fp, None)
-                self.assertEqual(fp.encoding, encoding)
-                self.assertEqual(fp.tell(), 0)
-                self.assertEqual(fp.readline(), '# test %s encoding\n'
-                                 % encoding)
+        if search_data['no_active'] != '':
+            sql += " and m.activationTime is NULL"
 
-        fp, filename, info = imp.find_module("tokenize")
-        with fp:
-            self.assertNotEqual(fp, None)
-            self.assertEqual(fp.encoding, "utf-8")
-            self.assertEqual(fp.tell(), 0)
-            self.assertEqual(fp.readline(),
-                             '"""Tokenization help for Python programs.\n')
-
-    def test_issue3594(
+        sql += ";"
+        return sql

d3653278dffcd6d5a6edea31b03385da65dfd41f
jimi_zhangao <mail_zhangao@126.com>
2017-12-01 15:21:36 +0800

    修改代码

--- pages/statistical_form/form_page.py
+++ pages/statistical_form/form_page.py
@@ -172,3 +172,34 @@ class FormPage(BasePage):
     def get_per_display_name_on_line_stay_not_shutdown(self, m):
         return self.driver.get_text(
             'x,/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/table/thead/tr/th[%s]/div[1]' % str(m + 1))
+
+    def click_display_line_button_acc(self):
+        self.driver.click_element('x,/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/button')
+        sleep(2)
+
+    def get_display_line_number_acc(self):
+        return len(list(self.driver.get_elements('x,/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/ul/li')))
+
+    def get_per_display_style_acc(self, n):
+        return self.driver.get_element(
+            'x,/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/ul/li[%s]/label/input' % str(
+                n + 1)).is_selected()
+
+    def get_per_display_name_acc(self, n):
+        return self.driver.get_element(
+            'x,/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/ul/li[%s]/label' % str(n + 1)).get_attribute(
+            'title')
+
+    def click_per_display_input_button_acc(self, n):
+        self.driver.click_element(
+            'x,/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/ul/li[%s]/label/input' % str(
+                n + 1))
+        sleep(1)
+
+    def get_display_line_name_number_acc(self):
+        return len(
+            list(self.driver.get_elements('x,/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/table/thead/tr/th')))
+
+    def get_per_display_name_on_line_acc(self, m):
+        return self.driver.get_text(
+            'x,/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/table/thead/tr/th[%s]/div[1]' % str(m + 1))
---
+++ testcases/statistical_form4/test_case_194_acc_verify_the_display_line.py
@@ -0,0 +1,74 @@
+import unittest
+from time import sleep
+from automate_driver.automate_driver import AutomateDriver
+from model.assert_text import AssertText
+from pages.base.base_page import BasePage
+from pages.base.lon_in_base import LogInBase
+from pages.statistical_form.form_page import FormPage
+from pages.statistical_form.statistic_form_page3 import StatisticFormPage3
+from pages.statistical_form.statistical_form_page import StatisticalFormPage
+
+
+class TestCase194AccVerifyTheDisplayLine(unittest.TestCase):
+    def setUp(self):
+        # 前置条件
+        # 实例化对象
+        self.driver = AutomateDriver()
+        self.base_url = self.driver.base_url
+        self.base_page = BasePage(self.driver, self.base_url)
+        self.statistical_form_page = StatisticalFormPage(self.driver, self.base_url)
+        self.statistical_form_page3 = StatisticFormPage3(self.driver, self.base_url)
+        self.log_in_base = LogInBase(self.driver, self.base_url)
+        self.assert_text = AssertText()
+        self.form_page = FormPage(self.driver, self.base_url)
+        # 打开页面，填写用户名、密码、点击登录
+        self.base_page.open_page()
+        self.driver.set_window_max()
+        self.driver.implicitly_wait(5)
+        self.driver.clear_cookies()
+        self.log_in_base.log_in_jimitest()
+
+        # 登录之后点击控制台，然后点击设置
+        self.statistical_form_page.click_control_after_click_statistical_form_page()
+        sleep(3)
+
+    def tearDown(self):
+        # 退出浏览器
+        self.driver.quit_browser()
+
+    def test_case_194_acc_verify_the_display_line(self):
+        # 断言url
+        expect_url_after_click_statistical_form = self.base_url + '/deviceReport/statisticalReport'
+        self.assertEqual(expect_url_after_click_statistical_form,
+                         self.statistical_form_page.actual_url_after_statistical_form())
+        # 断言
+        self.assertEqual(self.assert_text.statistical_form_sport_overview_form(),
+                         self.statistical_form_page.actual_text_after_click_sport_overview())
+        self.statistical_form_page.click_acc_form_button()
+        self.statistical_form_page.switch_to_acc_report_form_frame()
+        # 点击展示列
+        self.form_page.click_display_line_button_acc()
+        # 获取有多少个展示列
+        display_line_number = self.form_page.get_display_line_number_acc()
+        self.form_page.click_display_line_button_acc()
+        for n in range(display_line_number):
+            # 获取每一个展示列是否被勾选
+            self.form_page.click_display_line_button_acc()
+            display_style = self.form_page.get_per_display_style_acc(n)
+            display_line_name = self.form_page.get_per_display_name_acc(n)
+            # 点击
+            self.form_page.click_per_display_input_button_acc(n)
+            # 点击展示列
+            self.form_page.click_display_line_button_acc()
+            # 获取展示列数量
+            display_line_name_list = []
+            display_line_name_number = self.form_page.get_display_line_name_number_acc()
+            for m in range(display_line_name_number):
+                display_name = self.form_page.get_per_display_name_on_line_acc(m)
+                display_line_name_list.append(display_name)
+            if display_style == True:
+                self.assertNotIn(display_line_name, display_line_name_list)
+            elif display_style == False:
+                self.assertIn(display_line_name, display_line_name_list)
+
+        self.driver.default_frame()

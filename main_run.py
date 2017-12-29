f737672a46a74f93c659c15cf01af01e2ddf4ced
jimi_zhangao <mail_zhangao@126.com>
2017-12-29 18:14:37 +0800

    修改代码

--- main_run.py
+++ main_run.py
@@ -20,6 +20,18 @@ def run_04():
     os.system('python -m test_runner.tuqiangOL_test_runner_4_customer_management')
 
 
+def run_05():
+    os.system('python -m test_runner.tuqiangOL_test_runner_5_form_search')
+
+
+def run_06():
+    os.system('python -m test_runner.tuqiangOL_test_runner_6_form_port_search')
+
+
+def run_07():
+    os.system('python -m test_runner.tuqiangOL_test_runner_7_form_export')
+
+
 start_time = datetime.datetime(2017, 11, 28, 1, 10, 0)
 while datetime.datetime.now() < start_time:
     sleep(5)
@@ -40,6 +52,21 @@ for i4 in range(1):
     t4 = threading.Thread(target=run_04)
     t4.setDaemon(True)
     thread_list.append(t4)
+
+for i5 in range(1):
+    t5 = threading.Thread(target=run_05)
+    t5.setDaemon(True)
+    thread_list.append(t5)
+
+for i6 in range(1):
+    t6 = threading.Thread(target=run_06)
+    t6.setDaemon(True)
+    thread_list.append(t6)
+
+for i7 in range(1):
+    t7 = threading.Thread(target=run_07)
+    t7.setDaemon(True)
+    thread_list.append(t7)
 
 for t in thread_list:
     t.start()
